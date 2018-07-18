92daba91e696abf4ddcca3ca4f4e8a96cd8a8e60
Victor M. Suarez <svmnotn@gmail.com>
2018-07-18 19:21:43 -0400

    feat(webserver): add hello world presenting webserver
    Signed-off-by: Victor M. Suarez <svmnotn@gmail.com>

---
+++ webserver/src/main.rs
@@ -0,0 +1,12 @@
+#![feature(plugin)]
+#![plugin(rocket_codegen)]
+extern crate rocket;
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+fn main() {
+    rocket::ignite().mount("/", routes![index]).launch();
+}
