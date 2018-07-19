2f22f3b5f6b933f1844d26105634aa1ca94418a8
Victor M. Suarez <svmnotn@gmail.com>
2018-07-18 20:46:02 -0400

    feat(webserver): add tables.rs commencing the tables fun
    Signed-off-by: Victor M. Suarez <svmnotn@gmail.com>

--- webserver/src/main.rs
+++ webserver/src/main.rs
@@ -2,9 +2,18 @@
 #![plugin(rocket_codegen)]
 extern crate rocket;
 
+#[macro_use]
+extern crate serde_derive;
+extern crate serde;
+extern crate serde_json;
+
+extern crate rand;
+
+pub mod tables;
+
 #[get("/")]
 fn index() -> &'static str {
-    "Hello, world!"
+    "Hello, world! Get ready to make some Tables! :D"
 }
 
 fn main() {
