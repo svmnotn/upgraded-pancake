899ce943878f0251d55b08181cf8e7aa1b9171c0
Victor M. Suarez <svmnotn@gmail.com>
2018-07-20 16:09:41 -0400

    feat(server): add random "good" tables
    Signed-off-by: Victor M. Suarez <svmnotn@gmail.com>

--- server/src/main.rs
+++ server/src/main.rs
@@ -12,7 +12,13 @@ fn main() {
     rocket::ignite()
         .mount(
             "/",
-            routes![files::index, tables::get, tables::post, files::get,],
+            routes![
+                files::index,
+                tables::get,
+                tables::post,
+                tables::get_static,
+                files::get,
+            ],
         )
         .launch();
 }
--- server/src/tables.rs
+++ server/src/tables.rs
@@ -1,6 +1,6 @@
 extern crate rand;
 extern crate serde_json;
-
+use self::rand::{thread_rng, Rng};
 use rocket_contrib::Json;
 use upgraded_pancake::{Table, TableResult};
 
@@ -13,3 +13,19 @@ fn get() -> Json<Table> {
 fn post(table: Option<Json<Table>>) -> Option<Json<TableResult>> {
     table.and_then(|t| t.0.get()).map(|v| Json(v))
 }
+
+#[get("/table/static")]
+fn get_static() -> Json<Table> {
+    Json(
+        serde_json::from_str(thread_rng().choose(&CHOICES).expect("choices empty?"))
+            .expect("wrong json"),
+    )
+}
+
+const CHOICES: [&'static str; 5] = [
+    r#"{"dice":"1d6","results":[{"roll":1,"value":"DATA"},{"roll":2,"value":"DATA1"},{"roll":3,"value":"DATA2"},{"roll":4,"value":"DATA3"},{"roll":5,"value":"DATA4"},{"roll":6,"value":"DATA5"}]}"#,
+    r#"{"dice":{"amount":2,"size":6},"results":[{"roll":2,"value":"DATA"},{"range":"2-4","value":"DATA1"},{"roll":5,"value":"DATA3"},{"range":"6-10","value":"DATA4"},{"roll":11,"value":"DATA5"},{"roll":12,"value":"DATA6"}]}"#,
+    r#"{"dice":{"amount":1,"size":10},"results":[{"range":"1-3","value":"DATA"},{"roll":4,"value":"DATA1"},{"roll":5,"value":"DATA3"},{"roll":6,"value":"DATA4"},{"roll":7,"value":"DATA5"},{"range":"8-9","value":"DATA6"},{"roll":10,"value":"DATA6"}]}"#,
+    r#"{"dice":"3d4","results":[{"range":"3-4","value":"DATA"},{"range":"5-6","value":"DATA1"},{"range":"7-8","value":"DATA3"},{"range":"9-10","value":"DATA4"},{"range":"11-12","value":"DATA5"}]}"#,
+    r#"{"dice":"1d4","results":[{"roll":1,"value":"DATA"},{"roll":2,"value":"DATA1"},{"roll":3,"value":"DATA3"},{"roll":4,"value":"DATA4"}]}"#,
+];
