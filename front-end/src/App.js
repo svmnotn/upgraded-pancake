4f029ee791fa7e398fa05d6cedfb025bf992d54f
Shanna <ssit002@fiu.edu>
2018-07-21 15:33:58 -0400

    chore(front-end): organize and clean up front-end files

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,18 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 //import logo from './logo.svg';
-import './App.css';
+import './styles/App.css';
 
-class App extends Component {
+class App extends React.Component {
   render() {
     return (
-      <div className="Container">
+      <div>
         <h1>Upgraded Pancakes</h1>
-        
         <p>Claws in your leg leave fur on owners clothes. Love you, then bite you push your water glass on the floor or eat a rug and furry furry hairs everywhere oh no human coming lie on counter don't get off counter. Claws in your leg leaves on the floor or eat a rug and furry furry hairs everywhere oh </p>
+
         <div className="btnRow">
-          <button className="spaceRight">Generate</button>
-          <button>Create Table</button>
+          <button className="spaceRight">Roll</button>
+          <button className="ripple">Generate Table</button>
         </div>
+
       </div>
     );
   }
---
+++ front-end/src/components/ChooseComponent.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './App.css';
+
+class Choose extends React.Component {
+    render() {
+        return(
+            <div></div>
+        )
+    }
+}
