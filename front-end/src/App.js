b73421bfa2ad2cb900684462519c7cad375e6cae
Shanna <ssit002@fiu.edu>
2018-07-20 23:04:26 -0400

    feat(front-end): add react front-page

---
+++ front-end/src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+//import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="Container">
+        <h1>Upgraded Pancakes</h1>
+        
+        <p>Claws in your leg leave fur on owners clothes. Love you, then bite you push your water glass on the floor or eat a rug and furry furry hairs everywhere oh no human coming lie on counter don't get off counter. Claws in your leg leaves on the floor or eat a rug and furry furry hairs everywhere oh </p>
+        <div className="btnRow">
+          <button className="spaceRight">Generate</button>
+          <button>Create Table</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
