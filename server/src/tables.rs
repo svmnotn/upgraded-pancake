8c8e7351d2c3ffda2ad1f27a799daed533587520
Victor M. Suarez <svmnotn@gmail.com>
2018-07-28 20:13:58 -0400

    style(server): apply clippy fixes
    Signed-off-by: Victor M. Suarez <svmnotn@gmail.com>

--- server/src/tables.rs
+++ server/src/tables.rs
@@ -11,7 +11,7 @@ fn get() -> Json<Table> {
 
 #[post("/table", format = "application/json", data = "<table>")]
 fn post(table: Option<Json<Table>>) -> Option<Json<TableResult>> {
-    table.and_then(|t| t.0.get()).map(|v| Json(v))
+    table.and_then(|t| t.0.get()).map(Json)
 }
 
 #[get("/table/static")]
@@ -22,7 +22,7 @@ fn get_static() -> Json<Table> {
     )
 }
 
-const CHOICES: [&'static str; 6] = [
+const CHOICES: [&str; 6] = [
     r#"{"dice":"1d6","heading":"Test Data","results":[{"roll":1,"value":"DATA"},{"roll":2,"value":"DATA1"},{"roll":3,"value":"DATA2"},{"roll":4,"value":"DATA3"},{"roll":5,"value":"DATA4"},{"roll":6,"value":"DATA5"}]}"#,
     r#"{"dice":{"amount":2,"size":6},"heading":"Complex? Test Data","results":[{"roll":2,"value":"DATA"},{"roll":"2-4","value":"DATA1"},{"roll":5,"value":"DATA3"},{"roll":"6-10","value":"DATA4"},{"roll":11,"value":"DATA5"},{"roll":12,"value":"DATA6"}]}"#,
     r#"{"dice":{"amount":1,"size":10},"heading":"Problematic? Test Data2","results":[{"roll":"1-3","value":"DATA"},{"roll":4,"value":"DATA1"},{"roll":5,"value":"DATA3"},{"roll":6,"value":"DATA4"},{"roll":7,"value":"DATA5"},{"roll":"8-9","value":"DATA6"},{"roll":10,"value":"DATA6"}]}"#,
