d7a75d040ef5fb903ed4d6bfc046d28fa3c9a7e3
Victor M. Suarez <svmnotn@gmail.com>
2018-08-01 19:27:16 -0400

    chore(lib): prepare lib code for Rust 2018 Edition
    Signed-off-by: Victor M. Suarez <svmnotn@gmail.com>

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -1,9 +1,7 @@
+#![feature(rust_2018_preview)]
+
 #[macro_use]
 extern crate serde_derive;
-extern crate serde;
-extern crate serde_json;
-
-extern crate rand;
 
 pub const RNG_MIN_LIST_SIZE: usize = 2;
 pub const RNG_MAX_LIST_SIZE: usize = 15;
