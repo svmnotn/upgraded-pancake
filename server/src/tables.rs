680c3dc9387d1a0550678b79f95c9e285ab41207
Victor M. Suarez <svmnotn@gmail.com>
2018-08-18 15:03:18 -0400

    fix(server): fix the error in the static tables
    Signed-off-by: Victor M. Suarez <svmnotn@gmail.com>

--- server/src/tables.rs
+++ server/src/tables.rs
@@ -22,7 +22,7 @@ fn get_static() -> Json<Table> {
 
 const CHOICES: [&str; 6] = [
     r#"{"dice":"1d6","heading":"Test Data","results":[{"roll":1,"value":"DATA"},{"roll":2,"value":"DATA1"},{"roll":3,"value":"DATA2"},{"roll":4,"value":"DATA3"},{"roll":5,"value":"DATA4"},{"roll":6,"value":"DATA5"}]}"#,
-    r#"{"dice":{"amount":2,"size":6},"heading":"Complex? Test Data","results":[{"roll":2,"value":"DATA"},{"roll":"2-4","value":"DATA1"},{"roll":5,"value":"DATA3"},{"roll":"6-10","value":"DATA4"},{"roll":11,"value":"DATA5"},{"roll":12,"value":"DATA6"}]}"#,
+    r#"{"dice":{"amount":2,"size":6},"heading":"Complex? Test Data","results":[{"roll":2,"value":"DATA"},{"roll":"3-4","value":"DATA1"},{"roll":5,"value":"DATA3"},{"roll":"6-10","value":"DATA4"},{"roll":11,"value":"DATA5"},{"roll":12,"value":"DATA6"}]}"#,
     r#"{"dice":{"amount":1,"size":10},"heading":"Problematic? Test Data2","results":[{"roll":"1-3","value":"DATA"},{"roll":4,"value":"DATA1"},{"roll":5,"value":"DATA3"},{"roll":6,"value":"DATA4"},{"roll":7,"value":"DATA5"},{"roll":"8-9","value":"DATA6"},{"roll":10,"value":"DATA6"}]}"#,
     r#"{"dice":"3d4","heading":"Interesting? Test Data","results":[{"roll":"3-4","value":"DATA"},{"roll":"5-6","value":"DATA1"},{"roll":"7-8","value":"DATA3"},{"roll":"9-10","value":"DATA4"},{"roll":"11-12","value":"DATA5"}]}"#,
     r#"{"dice":"1d4","heading":"Hi it is Test Data","results":[{"roll":1,"value":"DATA"},{"roll":2,"value":"DATA1"},{"roll":3,"value":"DATA3"},{"roll":4,"value":"DATA4"}]}"#,
