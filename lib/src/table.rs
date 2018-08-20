fa19b51014ee11945bdb494ed1e109af367893b7
Victor M. Suarez <svmnotn@gmail.com>
2018-08-19 21:17:34 -0400

    feat(lib): remove random rolling of tables
    At least until I re-add it with a better algorithm for generating them
    
    Signed-off-by: Victor M. Suarez <svmnotn@gmail.com>

--- lib/src/table.rs
+++ lib/src/table.rs
@@ -4,12 +4,7 @@ pub use self::table_result::TableResult;
 #[cfg(test)]
 mod tests;
 
-use crate::{
-    gen_strings, Dice, Range, Row, Column, RNG_MAX_COL_SIZE, RNG_MAX_ROW_SIZE, RNG_MIN_COL_SIZE,
-    RNG_MIN_ROW_SIZE,
-};
-use rand::distributions::{Distribution, Standard};
-use rand::Rng;
+use crate::{Column, Dice, Range, Row};
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct Table {
@@ -62,51 +57,5 @@ impl Table {
             eprintln!("Not all values used!\n\t values: {:?}", values);
             false
         }
-    }
-}
-
-impl Distribution<Table> for Standard {
-    fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> Table {
-        fn gen_results<R: Rng + ?Sized>(
-            rng: &mut R,
-            dice: Dice,
-            columns: usize,
-            rows: usize,
-        ) -> Vec<Row> {
-            let mut results: Vec<Row> = Vec::with_capacity(rows);
-            for _ in 0..rows {
-                results.push(Row::new(
-                    if rng.gen() {
-                        dice.roll().into()
-                    } else {
-                        let init = rng.gen_range(dice.min(), dice.max() - 3);
-                        let finish = rng.gen_range(init + 1, dice.max() - 1);
-
-                        Range::from(
-                            rng.gen_range(init, finish)..=rng.gen_range(finish + 1, dice.max()),
-                        ).into()
-                    },
-                    gen_strings(columns, false),
-                ));
-            }
-
-            results.sort_unstable();
-
-            results
-        }
-
-        let dice: Dice = rng.gen();
-        let columns = rng.gen_range(RNG_MIN_COL_SIZE, RNG_MAX_COL_SIZE);
-        let rows = rng.gen_range(RNG_MIN_ROW_SIZE, RNG_MAX_ROW_SIZE);
-
-        let heading = gen_strings(columns, true);
-
-        let mut t = Table::new(dice, heading.clone(), gen_results(rng, dice, columns, rows));
-
-        while t.is_none() {
-            t = Table::new(dice, heading.clone(), gen_results(rng, dice, columns, rows));
-        }
-
-        t.expect("Loop broken before a valid Table was created?")
     }
 }
