c6728951307f0037a3188190899c791814099ffa
Victor M. Suarez <svmnotn@gmail.com>
2018-08-21 11:07:59 -0400

    feat(back-end): move const tables into their own files
    this should make it easier to edit and add new tests
    
    Signed-off-by: Victor M. Suarez <svmnotn@gmail.com>

--- lib/src/table/tests.rs
+++ lib/src/table/tests.rs
@@ -1,16 +1,22 @@
 use super::Table;
 
-const CHOICES: [&str; 6] = [
-    r#"{"dice":"1d6","heading":"Test Data","results":[{"roll":1,"value":"DATA"},{"roll":2,"value":"DATA1"},{"roll":3,"value":"DATA2"},{"roll":4,"value":"DATA3"},{"roll":5,"value":"DATA4"},{"roll":6,"value":"DATA5"}]}"#,
-    r#"{"dice":{"amount":2,"size":6},"heading":"Complex? Test Data","results":[{"roll":2,"value":"DATA"},{"roll":"3-4","value":"DATA1"},{"roll":5,"value":"DATA3"},{"roll":"6-10","value":"DATA4"},{"roll":11,"value":"DATA5"},{"roll":12,"value":"DATA6"}]}"#,
-    r#"{"dice":{"amount":1,"size":10},"heading":"Problematic? Test Data2","results":[{"roll":"1-3","value":"DATA"},{"roll":4,"value":"DATA1"},{"roll":5,"value":"DATA3"},{"roll":6,"value":"DATA4"},{"roll":7,"value":"DATA5"},{"roll":"8-9","value":"DATA6"},{"roll":10,"value":"DATA6"}]}"#,
-    r#"{"dice":"3d4","heading":"Interesting? Test Data","results":[{"roll":"3-4","value":"DATA"},{"roll":"5-6","value":"DATA1"},{"roll":"7-8","value":"DATA3"},{"roll":"9-10","value":"DATA4"},{"roll":"11-12","value":"DATA5"}]}"#,
-    r#"{"dice":"1d4","heading":"Hi it is Test Data","results":[{"roll":1,"value":"DATA"},{"roll":2,"value":"DATA1"},{"roll":3,"value":"DATA3"},{"roll":4,"value":"DATA4"}]}"#,
-    r#"{"dice":"1d6","heading":["Cool things","Stuffy stuff","is it all a lie?"],"results":[{"roll":1,"value":["I am cake","but that's a lie","cus the cake is a lie"]},{"roll":2,"value":["jerky","is neat","very neat"]},{"roll":3,"value":["cookies","very neat","we must eat"]},{"roll":4,"value":["we do","what we must","because we cam"]},{"roll":5,"value":["we all","lift","together"]},{"roll":6,"value":["we lift","for the grind","together"]}]}"#,
+const CHOICES: [&str; 12] = [
+    include_str!("../../../test_data/simple/value_single.json"),
+    include_str!("../../../test_data/simple/value_multiple.json"),
+    include_str!("../../../test_data/simple/ranges_single.json"),
+    include_str!("../../../test_data/simple/ranges_multiple.json"),
+    include_str!("../../../test_data/simple/mixed_single.json"),
+    include_str!("../../../test_data/simple/mixed_multiple.json"),
+    include_str!("../../../test_data/complex/value_single.json"),
+    include_str!("../../../test_data/complex/value_multiple.json"),
+    include_str!("../../../test_data/complex/ranges_single.json"),
+    include_str!("../../../test_data/complex/ranges_multiple.json"),
+    include_str!("../../../test_data/complex/mixed_single.json"),
+    include_str!("../../../test_data/complex/mixed_multiple.json"),
 ];
 
 #[test]
-fn valid_works_0() {
+fn simple_value_single_is_valid() {
     assert!(
         serde_json::from_str::<Table>(CHOICES[0])
             .expect("can't become table?")
@@ -19,7 +25,7 @@ fn valid_works_0() {
 }
 
 #[test]
-fn valid_works_1() {
+fn simple_value_multiple_is_valid() {
     assert!(
         serde_json::from_str::<Table>(CHOICES[1])
             .expect("can't become table?")
@@ -28,7 +34,7 @@ fn valid_works_1() {
 }
 
 #[test]
-fn valid_works_2() {
+fn simple_ranges_single_is_valid() {
     assert!(
         serde_json::from_str::<Table>(CHOICES[2])
             .expect("can't become table?")
@@ -37,7 +43,7 @@ fn valid_works_2() {
 }
 
 #[test]
-fn valid_works_3() {
+fn simple_ranges_multiple_is_valid() {
     assert!(
         serde_json::from_str::<Table>(CHOICES[3])
             .expect("can't become table?")
@@ -46,7 +52,7 @@ fn valid_works_3() {
 }
 
 #[test]
-fn valid_works_4() {
+fn simple_mixed_single_is_valid() {
     assert!(
         serde_json::from_str::<Table>(CHOICES[4])
             .expect("can't become table?")
@@ -55,9 +61,63 @@ fn valid_works_4() {
 }
 
 #[test]
-fn valid_works_5() {
+fn simple_mixed_multiple_is_valid() {
     assert!(
         serde_json::from_str::<Table>(CHOICES[5])
+            .expect("can't become table?")
+            .is_valid()
+    );
+}
+
+#[test]
+fn complex_value_single_is_valid() {
+    assert!(
+        serde_json::from_str::<Table>(CHOICES[6])
+            .expect("can't become table?")
+            .is_valid()
+    );
+}
+
+#[test]
+fn complex_value_multiple_is_valid() {
+    assert!(
+        serde_json::from_str::<Table>(CHOICES[7])
+            .expect("can't become table?")
+            .is_valid()
+    );
+}
+
+#[test]
+fn complex_ranges_single_is_valid() {
+    assert!(
+        serde_json::from_str::<Table>(CHOICES[8])
+            .expect("can't become table?")
+            .is_valid()
+    );
+}
+
+#[test]
+fn complex_ranges_multiple_is_valid() {
+    assert!(
+        serde_json::from_str::<Table>(CHOICES[9])
+            .expect("can't become table?")
+            .is_valid()
+    );
+}
+
+#[test]
+fn complex_mixed_single_is_valid() {
+    assert!(
+        serde_json::from_str::<Table>(CHOICES[10])
+            .expect("can't become table?")
+            .is_valid()
+    );
+}
+
+#[test]
+fn complex_mixed_multiple_is_valid() {
+    assert!(
+        serde_json::from_str::<Table>(CHOICES[11])
             .expect("can't become table?")
             .is_valid()
     );
--- server/src/tables.rs
+++ server/src/tables.rs
@@ -89,11 +89,17 @@ fn static_tables() -> Json<Table> {
     )
 }
 
-const CHOICES: [&str; 6] = [
-    r#"{"dice":"1d6","heading":"Test Data","results":[{"roll":1,"value":"DATA"},{"roll":2,"value":"DATA1"},{"roll":3,"value":"DATA2"},{"roll":4,"value":"DATA3"},{"roll":5,"value":"DATA4"},{"roll":6,"value":"DATA5"}]}"#,
-    r#"{"dice":{"amount":2,"size":6},"heading":"Complex? Test Data","results":[{"roll":2,"value":"DATA"},{"roll":"3-4","value":"DATA1"},{"roll":5,"value":"DATA3"},{"roll":"6-10","value":"DATA4"},{"roll":11,"value":"DATA5"},{"roll":12,"value":"DATA6"}]}"#,
-    r#"{"dice":{"amount":1,"size":10},"heading":"Problematic? Test Data2","results":[{"roll":"1-3","value":"DATA"},{"roll":4,"value":"DATA1"},{"roll":5,"value":"DATA3"},{"roll":6,"value":"DATA4"},{"roll":7,"value":"DATA5"},{"roll":"8-9","value":"DATA6"},{"roll":10,"value":"DATA6"}]}"#,
-    r#"{"dice":"3d4","heading":"Interesting? Test Data","results":[{"roll":"3-4","value":"DATA"},{"roll":"5-6","value":"DATA1"},{"roll":"7-8","value":"DATA3"},{"roll":"9-10","value":"DATA4"},{"roll":"11-12","value":"DATA5"}]}"#,
-    r#"{"dice":"1d4","heading":"Hi it is Test Data","results":[{"roll":1,"value":"DATA"},{"roll":2,"value":"DATA1"},{"roll":3,"value":"DATA3"},{"roll":4,"value":"DATA4"}]}"#,
-    r#"{"dice":"1d6","heading":["Cool things","Stuffy stuff","is it all a lie?"],"results":[{"roll":1,"value":["I am cake","but that's a lie","cus the cake is a lie"]},{"roll":2,"value":["jerky","is neat","very neat"]},{"roll":3,"value":["cookies","very neat","we must eat"]},{"roll":4,"value":["we do","what we must","because we cam"]},{"roll":5,"value":["we all","lift","together"]},{"roll":6,"value":["we lift","for the grind","together"]}]}"#,
+const CHOICES: [&str; 12] = [
+    include_str!("../../test_data/simple/value_single.json"),
+    include_str!("../../test_data/simple/value_multiple.json"),
+    include_str!("../../test_data/simple/ranges_single.json"),
+    include_str!("../../test_data/simple/ranges_multiple.json"),
+    include_str!("../../test_data/simple/mixed_single.json"),
+    include_str!("../../test_data/simple/mixed_multiple.json"),
+    include_str!("../../test_data/complex/value_single.json"),
+    include_str!("../../test_data/complex/value_multiple.json"),
+    include_str!("../../test_data/complex/ranges_single.json"),
+    include_str!("../../test_data/complex/ranges_multiple.json"),
+    include_str!("../../test_data/complex/mixed_single.json"),
+    include_str!("../../test_data/complex/mixed_multiple.json"),
 ];
