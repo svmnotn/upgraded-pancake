d8c51d4c911cf44f1a324bb08bacb39f43bc8cce
Victor M. Suarez <svmnotn@gmail.com>
2018-08-20 22:29:46 -0400

    docs(server): setup the guide for what the docs should look like
    Signed-off-by: Victor M. Suarez <svmnotn@gmail.com>

--- server/src/main.rs
+++ server/src/main.rs
@@ -18,16 +18,27 @@
 //! * **GET** `/table/all/data`: Used to retrieve all available tables as JSON
 //! * **POST** `/table/validate`: Used to make sure the included table is valid
 //!
-//! The following are methods that _might_ be removed in the future
+//! The following are HTTP methods that _might_ be removed in the future
 //! * **GET** `/table/static`: Used to obtain a table from a selection of premade tables,
 //! useful for having sane defaults
 //! * **POST** `/table`: Used to roll on the included table
+//!
+//! The following are the HTTP methods that are used so that the server can host a fully
+//! functional website:
+//! * **GET** `/`: Used to obtain the index file, this should be a normal html file located
+//! `front-end/build/index.html` as per the writing of this documentation
+//! * **GET** `/<files>`: Used to obtain all other files that are referenced from the index,
+//! the default path for the files is `front-end/build` as per the writting of this documentation
 //!
 //! ### Adding a table
 //!
 //! To add a table to the storage one must make a **HTTP PUT** method call to `/table/<name>`
 //! where `name` is the desired identifier for the table. _Furthermore_, the table to add
 //! must be sent as JSON in the data portion of the HTTP method call.
+//!
+//! The server will then return `true` if the table was correctly added, and `false` if it was
+//! not. As of writting this documentation, the only way for a table to be denied is for it
+//! to not be valid.
 
 mod files;
 mod tables;
